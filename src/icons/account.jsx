37471f995eff643ba57f8e286becd85cbc5bfa3f
Vishnu T C <111764448+TCvishnu@users.noreply.github.com>
2024-03-26 13:41:41 +0530

    Delete src/icons directory

--- src/icons/account.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Account(props) {
-  return (
-    <div className={props.styling}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" viewBox="0 0 24 24" fill="none" stroke="#062d53" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-user"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="10" r="3"/><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662"/></svg>
-    </div>
-  )
-}
